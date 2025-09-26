9fb6d29ed1b12f24f7fc28e10620983a4fa97238
Safoun <safoun.official@gmail.com>
2025-09-26 10:52:21 +0600

    learning the basics and fundamentals

---
+++ even_odd/even_odd.c
@@ -0,0 +1,15 @@
+// this c program checks whether a number is even or odd
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer: ");
+
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+        printf("%d is even.\n", num);
+    else
+        printf("%d is odd.\n", num);
+        
+    return 0;
+}
