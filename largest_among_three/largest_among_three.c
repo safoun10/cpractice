8667b4c3d8f692fc956b07b49968dfd6282a70a8
Safoun <safoun.official@gmail.com>
2025-09-27 10:13:14 +0600

    size proofed this, limit is positive and negative 9 quintillion

--- largest_among_three/largest_among_three.c
+++ largest_among_three/largest_among_three.c
@@ -2,30 +2,29 @@
 
 int main()
 {
-    int first, second, third;
+    long long int first, second, third;
 
     // flush the input buffer to avoid any input except integers
 
     printf("\nThis program finds the largest among three numbers.\n");
-    printf("Please enter three integers (positive, negative or zero):\n");
-    printf("The limit for inputs is from 2 billion to -2 billion.\n");
+    printf("Please enter three integers (positive, negative or zero)\n");
     printf("----------------------------------------------------------------------\n");
     printf("Enter first number: ");
-    if (scanf("%d", &first) != 1)
+    if (scanf("%lld", &first) != 1)
     {
         printf("Invalid first input! Please enter an integer.\n");
         return 1; // stop program
     }
 
     printf("Enter second number: ");
-    if (scanf("%d", &second) != 1)
+    if (scanf("%lld", &second) != 1)
     {
         printf("Invalid second input! Please enter an integer.\n");
         return 1; // stop program
     }
 
     printf("Enter third number: ");
-    if (scanf("%d", &third) != 1)
+    if (scanf("%lld", &third) != 1)
     {
         printf("Invalid third input! Please enter an integer.\n");
         return 1; // stop program
@@ -34,19 +33,19 @@ int main()
     printf("----------------------------------------------------------------------\n");
     // logic to find the largest number
     if (first > second && first > third)
-        printf("%d ~ the first number is the largest number.", first);
+        printf("%lld ~ the first number is the largest number.", first);
     else if (second > first && second > third)
-        printf("%d ~ the second number the largest number.", second);
+        printf("%lld ~ the second number the largest number.", second);
     else if (third > first && third > second)
-        printf("%d ~ the third number is the largest number.", third);
+        printf("%lld ~ the third number is the largest number.", third);
     else if (first == second && first == third)
         printf("All numbers are equal.");
     else if (first == second && first > third)
-        printf("%d ~ the first and %d ~ the second number are the largest numbers.", first, second);
+        printf("%lld ~ the first and %lld ~ the second number are the largest numbers.", first, second);
     else if (first == third && first > second)
-        printf("%d ~ the first and %d ~ the third number are the largest numbers.", first, third);
+        printf("%lld ~ the first and %lld ~ the third number are the largest numbers.", first, third);
     else if (second == third && second > first)
-        printf("%d ~ the second and %d ~ the third number are the largest numbers.", second, third);
+        printf("%lld ~ the second and %lld ~ the third number are the largest numbers.", second, third);
     else
         printf("This program should not reach here!");
 
