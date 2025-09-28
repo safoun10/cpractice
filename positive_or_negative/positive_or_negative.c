c0fa4980d6c8b148f408d65ecfd699d11e59a645
Safoun <safoun.official@gmail.com>
2025-09-28 10:36:55 +0600

    positive or negative

---
+++ positive_or_negative/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if (number >= 0)
+        printf("%d is positive.\n", number);
+    else
+        printf("%d is negative.\n", number);
+
+    return 0;
+}
